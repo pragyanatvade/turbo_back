6d0afabe06f33dd477b71dc484e8380abf6611f9
Pragyan Tripathi <pragyan@vadelabs.com>
2020-11-22 14:35:47 +0530

    :tada: add devcard support for writing cards and tests

--- card/com/vadelabs/turbo/ui/layout_card.cljs
+++ card/com/vadelabs/turbo/ui/layout_card.cljs
@@ -1,13 +1,84 @@
 (ns com.vadelabs.turbo.ui.layout-card
   (:require
    [devcards.core :as dc]
-   [com.vadelabs.turbo.dom :as dom :refer [defui]])
+   [com.vadelabs.turbo.dom :as dom :refer [defui]]
+   [com.vadelabs.turbo.ui.layout :as ui])
   (:require-macros
    [devcards.core :refer [defcard]]))
 
-(defui Hello
+(defui BasicUI
   []
-  [:h1 "HELLO COMPONEN"])
+  [:& ui/Box {}
+   [:& ui/Box {:color ["tomato" "purple"]
+               :hover {:bg    "red.500"
+                       :color "white"}
+               } "Welcome to Box"]
+   [:& ui/Box {:position "relative"
+               :bg       "red.200"
+               :before   {:height         0
+                          :content        "\"\""
+                          :display        "block"
+                          :padding-bottom ["40px" "100px"]}}]])
+(defcard Basic BasicUI)
 
-(defcard Hello Hello)
+(defui SquareUI
+  []
+  [:& ui/Square {:bg   "red.300"
+                 :size ["40px" "60px" "100px"]}
+   [:& ui/Circle {:size  "60px"
+                  :bg    "tomato"
+                  :color "white"}
+    "BOX"]])
+
+(defcard Square SquareUI)
+
+(defui CenterUI
+  []
+  [:& ui/Center {:bg "#da3d6929"
+                 :w  "480px"
+                 :h  "400px"}
+   [:& ui/Box {:p  "40px"
+               :bg "green.600"}
+    "BOX"]])
+
+(defcard Center CenterUI)
+
+(defui SpacerUI
+  []
+  [:& ui/Flex {:color     ["green.400" "tomato"]
+               :direction {:base "column" :md "row"}}
+   [:& ui/Box {:bg       "pink.500"
+               :box-size "100px"}
+    "Box 1"]
+   [:& ui/Spacer]
+   [:& ui/Box {:bg       "green.500"
+               :box-size "100px"}
+    "Box 2"]])
+
+(defcard Spacer SpacerUI)
+
+(defui CodeUI
+[]
+[:& ui/Code {:color-scheme "pink"} "import React from react"])
+(defcard Code CodeUI)
+
+(defui ContainerUI
+[]
+[:& ui/Container {:max-width "md"}
+ [:p "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum."]])
+(defcard Container ContainerUI)
+
+(defui DividerUI
+[]
+[:& ui/Divider])
+
+(defcard Divider DividerUI)
 
--- test/com/vadelabs/turbo/styled/parser_test.cljs
+++ test/com/vadelabs/turbo/styled/parser_test.cljs
@@ -3,8 +3,93 @@
    [devcards.core :as dc]
    [com.vadelabs.turbo.styled.parser :as p])
   (:require-macros
-   [devcards.core :as dc :refer [defcard deftest]]
+   [devcards.core :as dc :refer [deftest]]
    [cljs.test :refer [is testing]]))
 
-(deftest TEST
-  (is (= 1 1)))
+(deftest background
+  (testing "returns background styles"
+    (let [style (p/background {:background-image "url(kitten.gif)"} p/style-keys p/pseudo-keys)]
+      (is (= style {:background-image "url(kitten.gif)"})))))
+
+(deftest border
+  (testing "returns border styles"
+    (let [style (p/border {:border "1px solid gold"} p/style-keys p/pseudo-keys)]
+      (is (= style {:border "1px solid gold"}))))
+  (testing "returns individual border styles"
+    (let [style (p/border {:theme                      {:border-widths {:thin 1}
+                                                        :colors        {:primary "red"}
+                                                        :border-styles {:thick "solid"}
+                                                        :radii         {:small 5}}
+                           :border-top-width           "thin"
+                           :border-top-color           "primary"
+                           :border-top-style           "thick"
+                           :border-top-left-radius     "small"
+                           :border-top-right-radius    "small"
+                           :border-bottom-width        "thin"
+                           :border-bottom-color        "primary"
+                           :border-bottom-style        "thick"
+                           :border-bottom-left-radius  "small"
+                           :border-bottom-right-radius "small"
+                           :border-right-width         "thin"
+                           :border-right-color         "primary"
+                           :border-right-style         "thick"
+                           :border-left-width          "thin"
+                           :border-left-color          "primary"
+                           :border-left-style          "thick"}
+                          p/style-keys
+                          p/pseudo-keys)]
+      (is (= style {:border-top-color           "red"
+                    :border-top-width           1
+                    :border-top-style           "solid"
+                    :border-top-left-radius     5
+                    :border-top-right-radius    5
+                    :border-bottom-color        "red"
+                    :border-bottom-width        1
+                    :border-bottom-style        "solid"
+                    :border-bottom-left-radius  5
+                    :border-bottom-right-radius 5
+                    :border-right-color         "red"
+                    :border-right-width         1
+                    :border-right-style         "solid"
+                    :border-left-color          "red"
+                    :border-left-width          1
+                    :border-left-style          "solid"})))
+    (testing "returns border top and bottom radii"
+      (let [style (p/border {:theme                      {:radii {:small 5}}
+                             :border-top-left-radius     "small"
+                             :border-top-right-radius    "small"
+                             :border-bottom-left-radius  "small"
+                             :border-bottom-right-radius "small"}
+                            p/style-keys
+                            p/pseudo-keys)]
+        (is (= style {:border-top-left-radius     5
+                      :border-top-right-radius    5
+                      :border-bottom-left-radius  5
+                      :border-bottom-right-radius 5}))))))
+
+(deftest color
+  (testing "returns colors styles"
+    (let [style (p/color {:color "gold"
+                          :fill  "tomato"}
+                         p/style-keys
+                         p/pseudo-keys)]
+      (is (= style {:color "gold"
+                    :fill  "tomato"})))))
+
+(deftest position
+  (testing "returns position styles"
+    (let [style (p/position {:position "absolute" :top 0 :right 0}
+                            p/style-keys
+                            p/pseudo-keys)]
+      (is (= style {:position "absolute" :top 0 :right 0}))))
+  (testing "returns theme values"
+    (let [style (p/position {:theme {:space [0 4 8 16 32 64 128 256 512]}
+                             :top   1 :right 2 :bottom 3 :left 4}
+                            p/style-keys
+                            p/pseudo-keys)]
+      (is (= style {:top 4 :right 8 :bottom 16 :left 32}))))
+  (testing "returns pixel values"
+    (let [style (p/position {:top "1px" :right "2px" :bottom "3px" :left "4px"}
+                            p/style-keys
+                            p/pseudo-keys)]
+      (is (= style {:top "1px" :right "2px" :bottom "3px" :left "4px"})))))
