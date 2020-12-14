93c0737023802c59ab88ed7836c8c2e7dd00c274
Pragyan Tripathi <pragyan@vadelabs.com>
2020-12-14 21:25:41 +0530

    :recycle: re-add tag devcards

--- card/com/vadelabs/turbo/ui/tag_card.cljs
+++ card/com/vadelabs/turbo/ui/tag_card.cljs
@@ -8,51 +8,46 @@
   (:require-macros
    [devcards.core :refer [defcard]]))
 
-;; (defcard BasicTag (ui/tag "Gray"))
+(defcard BasicTag (ui/tag "Gray"))
 
-;; (defcard WithSizes (<>
-;;                     (ui/tag {:size "sm"} "Gray")
-;;                     (ui/tag {:size "md"} "Gray")
-;;                     (ui/tag {:size "lg"} "Gray")))
+(defcard WithSizes (<>
+                    (ui/tag {:size "sm"} "Gray")
+                    (ui/tag {:size "md"} "Gray")
+                    (ui/tag {:size "lg"} "Gray")))
 
-;; (defcard ColorSchemes (<>
-;;                        (ui/tag {:size "sm" :color-scheme "green"} "Green")
-;;                        (ui/tag {:size "md" :color-scheme "pink"} "Pink")
-;;                        (ui/tag {:size "lg" :color-scheme "blue"} "Blue")))
+(defcard ColorSchemes (<>
+                       (ui/tag {:size "sm" :color-scheme "green"} "Green")
+                       (ui/tag {:size "md" :color-scheme "pink"} "Pink")
+                       (ui/tag {:size "lg" :color-scheme "blue"} "Blue")))
 
-;; (defcard WithLeftIcon (ui/tag
-;;                        {:color-scheme "cyan"}
-;;                        (ui/left-icon {:w "12px" :h "12px" :as icons/add})
-;;                        (ui/label "Green")))
+(defcard WithLeftIcon (ui/tag
+                       {:color-scheme "cyan"}
+                       (ui/left-icon {:w "12px" :h "12px" :as icons/add})
+                       (ui/label "Green")))
 
-;; (defcard WithRightIcon (<>
-;;                         (ui/tag
-;;                          {:color-scheme "cyan"}
-;;                          (ui/label "Green")
-;;                          (ui/right-icon {:w "12px" :h "12px" :as icons/add}))
-;;                         (ui/tag
-;;                          {:variant "solid" :color-scheme "teal"}
-;;                          (ui/label "Teal")
-;;                          (ui/right-icon {:as icons/add}))))
+(defcard WithRightIcon (<>
+                        (ui/tag
+                         {:color-scheme "cyan"}
+                         (ui/label "Green")
+                         (ui/right-icon {:w "12px" :h "12px" :as icons/add}))
+                        (ui/tag
+                         {:variant "solid" :color-scheme "teal"}
+                         (ui/label "Teal")
+                         (ui/right-icon {:as icons/add}))))
 
-;; TODO Fix close-button styling - it should be a transparent cross
 (defcard WithCloseButton (<>
-                          ;; (ui/tag
-                          ;;  {:variant "solid" :size "sm" :color-scheme "cyan"}
-                          ;;  (ui/label "Tag Label")
-                          ;;  (ui/close-button)
-                          ;;  )
-                          ;; (ui/tag
-                          ;;  {:variant "solid" :size "md" :color-scheme "cyan"}
-                          ;;  (ui/label "Tag Label")
-                          ;;  (ui/close-button)
-                          ;;  )
+                          (ui/tag
+                           {:variant "solid" :size "sm" :color-scheme "cyan"}
+                           (ui/label "Tag Label")
+                           (ui/close-button))
+                          (ui/tag
+                           {:variant "solid" :size "md" :color-scheme "cyan"}
+                           (ui/label "Tag Label")
+                           (ui/close-button))
                           (ui/tag
                            {:variant "solid" :size "lg" :color-scheme "cyan"}
                            (ui/label "Tag Label")
-                           (ui/close-button)
-                           )
-                          ))
+                           (ui/close-button))))
 ;; (defcard WithCustomElement (ui/tag
 ;;                             {:size "lg" :color-scheme "red" :border-radius "full"}
 ;;                             (d/div "AVATAR")
